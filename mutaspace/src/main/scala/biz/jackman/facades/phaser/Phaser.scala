1d1757149010292b963c553c849ae7e96c7a9b80
Ben Jackman <ben@jackman.biz>
2015-07-23 20:40:43 -0500

    Moved union to it's own location
    Signed-off-by: Ben Jackman <ben@jackman.biz>

--- mutaspace/src/main/scala/biz/jackman/facades/phaser/Phaser.scala
+++ mutaspace/src/main/scala/biz/jackman/facades/phaser/Phaser.scala
@@ -17,58 +17,7 @@ import scala.scalajs.js.annotation.JSName
 //////////////////////////////////////////////////////////////
 
 
-object | {
-  implicit def t1ToU2[T](x: T): T | Nothing = x.asInstanceOf[T | Nothing]
-  implicit def t2ToU2[T](x: T): Nothing | T = x.asInstanceOf[Nothing | T]
-
-  implicit def t1ToU3[T](x: T): T | Nothing | Nothing = x.asInstanceOf[T | Nothing | Nothing]
-  implicit def t2ToU3[T](x: T): Nothing | T | Nothing = x.asInstanceOf[Nothing | T | Nothing]
-
-  implicit def t1ToU4[T](x: T): T | Nothing | Nothing | Nothing = x.asInstanceOf[T | Nothing | Nothing | Nothing]
-  implicit def t2ToU4[T](x: T): Nothing | T | Nothing | Nothing = x.asInstanceOf[Nothing | T | Nothing | Nothing]
-}
-
-trait |[+T1, +T2] extends js.Object
-
-
-class A
-class B
-class C
-class D
-
-trait Foo extends js.Object {
-  def ab(x: A | B): js.Any = js.native
-  def abc(x: A | B | C): js.Any = js.native
-  def abcd(x: A | B | C | D): js.Any = js.native
-}
-
-object Bar {
-
-  val a = new A
-  val b = new B
-  val c = new C
-  val d = new D
-
-  val x: Foo = null
-
-  x.ab(a) //Compiles
-  x.ab(b) //This too
-
-  //  x.stringOrInt(5.0) //Nope
-
-  x.abc(a) //Ok
-  x.abc(b) //Ok
-  x.abc(c) //Ok
-
-  x.abcd(a) //Ok
-  x.abcd(b) //Ok
-  x.abcd(c) //Ok
-  x.abcd(d) //Ok
-}
-
-
 object Phaser {
-
 
   trait Renderer extends js.Object
 
